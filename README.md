31c7e3aa84c4122fa6f8489a9e8523c8d83a3d4d
xlebpushek <81622486+xlebpushek@users.noreply.github.com>
2021-12-17 03:06:53 +0000

    Reconfiguration and minor edits

---
+++ README.md
@@ -0,0 +1,4 @@
+### Preview
+
+**Working** <br><br>
+![Something went wrong. Image not loaded](https://github.com/xlebpushek/gulp-configuration/blob/main/assets/preview/working.png) <br><br>
--- gulp-configuration/gulpfile.js
+++ gulp-configuration/gulpfile.js
@@ -1,117 +1,146 @@
-// ### Connecting modules
-// ## General
+const fs = require('fs');
+const path = require('path')
+const colors = require("colors")
 const argv = require("minimist")(process.argv.slice(2), {
-  string: ["path"],
-  boolean: ["production", "development"],
-  alias: {
-    "production": "p",
-    "development": "d"
-  },
-  default: {
-    "production": false,
-    "production": false
-  },
-  unknown: (arg) => {
-    console.error(colors.red("Unknown flag: ") + arg)
-    process.exit()
-  },
+    string: ["path", "port", "uiport"],
+    boolean: ["production", "development"],
+    default: {
+        production: false,
+        production: false
+    },
+    unknown: (arg) => {
+        console.error("Unknown flag:".red, (arg).bold)
+        process.exit(1)
+    }
 })
-const colors = require('ansi-colors')
-// ## Gulp
-// # General
+
 const { src, dest, watch, series, parallel } = require("gulp")
+
 const when = require("gulp-if")
 const sourcemaps = require("gulp-sourcemaps")
+const preprocess = require("gulp-preprocess")
 const concatenation = require("gulp-concat")
-// # html
-const fileinclude = require("gulp-file-include")
-// # styles
-const sass = require("gulp-sass")(require("sass"))
-// # scripts
-const rigger = require("gulp-rigger")
-// # server
+
+const pages_file_include = require("gulp-file-include")
+const pages_minifier = require('gulp-htmlmin');
+
+const styles_sass = require("gulp-sass")(require("sass"))
+const scripts_rigger = require("gulp-rigger")
+
 const browserSync = require("browser-sync").create()
 
-// ### Variables
-const root = argv.path || process.env.PWD
-const source = root + "/.source"
+const settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'))
 
+const root = argv.root || settings.root || process.env.PWD
+const source = path.join(root, ".source")
 
-class Tasks {
-  
-  constructor() {
-    
-    const base = parallel(this.html, this.styles, this.scripts)
-    
-    if (argv.development) {
-        exports.default = series(base, this.server)
-    } else {
-        exports.default = base
+const paths = {
+    source: {
+        pages: source + "/pages/**/index.html",
+        styles: source + "/styles/*.scss",
+        scripts: source + "/scripts/*.js",
+        json: source + "/json/*.json",
+        fonts: source + "/assets/fonts/*.*",
+        audio: source + "/assets/media/audio/*.*",
+        images: source + "/assets/media/images/*.*",
+        video: source + "/assets/media/video/*.*",
+    },
+    build: {
+        pages: root + "/",
+        styles: root + "/styles/",
+        scripts: root + "/scripts/",
+        json: root + "/json/",
+        fonts: root + "/assets/fonts/",
+        audio: root + "/assets/media/audio/",
+        images: root + "/assets/media/images/",
+        video: root + "/assets/media/video/",
     }
-    
-  }
-
-  html() {
-    
-    return src(source + "/html/*.html")
-    .pipe(fileinclude())
-    .pipe(dest(root))
-    .pipe(when(argv.devServ, browserSync.stream()))
-    
-  }
-  
-  styles() {
-    
-    return src(source + "/styles/*.scss")
-    .pipe(when(argv.dev, sourcemaps.init()))
-	    .pipe(sass())
-	    // .pipe(concatenation("style.css"))
-    .pipe(when(argv.dev, sourcemaps.write(".")))
-    .pipe(dest(root + "/styles/"))
-    .pipe(when(argv.devServ, browserSync.stream()))
-    
-  }
-  
-  scripts() {
-    
-    return src(source + "/scripts/*.js")
-    .pipe(when(argv.dev, sourcemaps.init()))
-	    // .pipe(concatenation("script.js"))
-	    .pipe(rigger())
-    .pipe(when(argv.dev, sourcemaps.write(".")))
-    .pipe(dest(root + "/scripts"))
-    .pipe(when(argv.devServ, browserSync.stream()))
-    
-  }
-  
-  server() {
-    
-    browserSync.init({
-      server: {
-        baseDir: root,
-      },
-      port: 4307,
-      ui: {
-        port: 8080,
-      },
-      ghostMode: {
-        clicks: true,
-        scroll: false,
-        location: false,
-        forms: false,
-      },
-      logPrefix: "Web",
-      open: false,
-      reloadOnRestart: true,
-      notify: false,
-    })
-
-    watch([source + "/html/*.html"], tasks.html)
-    watch([source + "/styles/*.scss"], tasks.styles)
-    watch([source + "/scripts/*.js"], tasks.scripts)
-  }
-  
 }
 
 
-const tasks = new Tasks()
+function pages() {
+    return src(paths.source.pages)
+        .pipe(when(argv.production, sourcemaps.init()))
+        .pipe(pages_file_include())
+        .pipe(when(
+            argv.development,
+            preprocess({
+                context: {
+                    mode: "development",
+                }
+            })
+        ))
+        .pipe(when(
+            argv.production,
+            pages_minifier({
+                collapseWhitespace: true,
+                keepClosingSlash: true,
+                removeAttributeQuotes: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                removeEmptyElements: true,
+                removeOptionalTags: true,
+                useShortDoctype: true
+            })
+        ))
+        .pipe(when(argv.production, sourcemaps.write(".")))
+        .pipe(dest(paths.build.pages))
+        .pipe(when(argv.development, browserSync.stream()))
+}
+
+function styles() {
+    return src(paths.source.styles)
+        .pipe(when(argv.production, sourcemaps.init()))
+        .pipe(styles_sass())
+        .pipe(when(argv.production, sourcemaps.write(".")))
+        .pipe(dest(paths.build.styles))
+        .pipe(when(argv.development, browserSync.stream()))
+}
+
+function scripts() {
+    return src(paths.source.scripts)
+        .pipe(when(argv.production, sourcemaps.init()))
+        .pipe(scripts_rigger())
+        .pipe(when(argv.production, sourcemaps.write(".")))
+        .pipe(dest(paths.build.scripts))
+        .pipe(when(argv.development, browserSync.stream()))
+}
+
+function server() {
+    browserSync.init({
+        server: {
+            baseDir: root,
+        },
+        port: argv.port || settings.port,
+        ui: {
+            port: argv.uiport || settings.uiport,
+        },
+        logPrefix: "gulp-configuration",
+        open: false,
+        reloadOnRestart: true,
+    })
+
+    watch(paths.source.pages, pages)
+    watch(paths.source.styles, styles)
+    watch(paths.source.scripts, scripts)
+}
+
+function main() {
+    const production = parallel(pages, styles, scripts)
+    const development = series(production, server)
+    
+    if (!argv.production && !argv.development) {
+        if (settings.mode != "--production" && settings.mode != "--development") {
+          console.error("Unknown flag:".red, (settings.mode).bold)
+          process.exit(1)
+        }
+    }
+    
+    if (settings.mode == "--production" || argv.production) {
+        exports.default = production
+    } else if (settings.mode == "--development" || argv.development) {
+        exports.default = development
+    }
+}
+
+main()
